e635b7819067189cd4082b2745c2670dfcc6bcb4
Tape Dispenser <gramiakshaye@gmail.com>
2023-07-16 00:35:09 -0700

    completely redid pickSong() to use the new biome playlists system

--- src/main/java/net/fabricmc/mcmp/MCMP_main.java
+++ src/main/java/net/fabricmc/mcmp/MCMP_main.java
@@ -13,9 +13,11 @@ import net.minecraft.sound.SoundEvent;
 import net.fabricmc.api.ModInitializer;
 
 
+import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import static net.fabricmc.mcmp.config_manager.biomes;
 import static net.fabricmc.mcmp.register_resources.*;
 import net.fabricmc.mcmp.config_manager;
 
@@ -49,6 +51,8 @@ public class MCMP_main implements ModInitializer {
 	public static PositionedSoundInstance nowPlaying() {
 		return currentlyPlaying;
 	}
+
+	public static Random song_rng = new Random();
 	
 	// literally just the vanilla playSong function but it won't be called by vanilla minecraft...
 	public static void playSong(SoundEvent songToPlay) {
@@ -65,46 +69,17 @@ public class MCMP_main implements ModInitializer {
 	public static SoundEvent pickSong() {
 		if (!inMainMenu()) {
 			String biome = mc.world.getBiome(mc.player.getBlockPos()).getKey().get().getValue().toString();
-			switch(biome) {
-				case "minecraft:basalt_deltas":
-					return SoundEvent.of(BASALT_DELTAS_1);
-				case "minecraft:birch_forest":
-					return SoundEvent.of(BIRCH_FOREST_1);
-				case "minecraft:cherry_forest":
-					return SoundEvent.of(CHERRY_FOREST_1);
-				case "minecraft:dark_oak_forest":
-					return SoundEvent.of(DARK_OAK_FOREST_1);
-				case "minecraft:deep_dark":
-					return SoundEvent.of(DEEP_DARK_1);
-				case "minecraft:desert":
-					return SoundEvent.of(DESERT_1);
-				case "minecraft:forest":
-					return SoundEvent.of(FOREST_1);
-				case "minecraft:jungle":
-					return SoundEvent.of(JUNGLE_1);
-				case "minecraft:lush_caves":
-					return SoundEvent.of(LUSH_CAVES_1);
-				case "minecraft:mountain":
-					return SoundEvent.of(MOUNTAIN_1);
-				//case "minecraft:mountains_meadows":
-				//	return SoundEvent.of(MOUNTAINS_MEADOWS_1);
-				case "minecraft:mushroom_island":
-					return SoundEvent.of(MUSHROOM_ISLAND_1);
-				case "minecraft:nether_wastes":
-					return SoundEvent.of(NETHER_WASTES_1);
-				case "minecraft:plains":
-					return SoundEvent.of(PLAINS_1);
-				case "minecraft:snow_plains":
-					return SoundEvent.of(SNOW_PLAINS_1);
-				case "minecraft:soul_sand_valley":
-					return SoundEvent.of(SOUL_SAND_VALLEY_1);
-				case "minecraft:taiga":
-					return SoundEvent.of(TAIGA_1);
-					case "minecraft:warm_ocean":
-					return SoundEvent.of(WARM_OCEAN_1);
-				default:
-					return SoundEvent.of(MY_SOUND_ID);
+			Vector<String> playlist = biomes.get(biome);
+			if (playlist.size() == 0){
+				playlist = biomes.get("fallback");
 			}
+			String song = playlist.get(song_rng.nextInt() % playlist.size()).replaceAll("\"", "");
+			if (song.equals("vanilla")) {
+				song = biomes.get(song).get(song_rng.nextInt() % playlist.size()).replaceAll("\"", "");  // have fun understanding this when i'm not on a violent amount of cannabis
+			}
+			MCMP_main.LOGGER.info(biome.concat(", ".concat(song)));
+			return SoundEvent.of(Identifier.tryParse(song));
+
 		} else {
 			// menu music
 			return SoundEvent.of(MENU_1);
