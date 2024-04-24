99a29f385ac3d8dfbb63ebe33f90634ef1a2b35c
Tape Dispenser <gramiakshaye@gmail.com>
2024-04-24 12:16:12 -0600

    remove unnecessary dependencies

--- src/client/java/net/tape/timm/gui/widget/configCheckbox.java
+++ src/client/java/net/tape/timm/gui/widget/configCheckbox.java
@@ -1,21 +1,17 @@
 package net.tape.timm.gui.widget;
 
-import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
 import net.minecraft.client.gui.screen.narration.NarrationPart;
 import net.minecraft.client.gui.widget.PressableWidget;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.MathHelper;
 import net.tape.timm.configManager;
 import net.tape.timm.modConfig;
-import net.tape.timm.timmMain;
+
 
 @Environment(value=EnvType.CLIENT)
 public class configCheckbox extends PressableWidget {
